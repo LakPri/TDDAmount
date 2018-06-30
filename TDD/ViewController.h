e9bd77bd74d0b9ca3624d27d2126546403a5e644
Lakshmi <lakshmi@gowthams-mbp.lan>
2018-06-30 19:39:11 +0200

    Initial Commit

---
+++ TDD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TDD
+//
+//  Created by Lakshmi on 6/30/18.
+//  Copyright © 2018 Lakshmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TDD/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TDD
+//
+//  Created by Lakshmi on 6/30/18.
+//  Copyright © 2018 Lakshmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
